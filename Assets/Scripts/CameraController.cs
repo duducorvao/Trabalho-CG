1c846839d58ef2c64401c4fb5ec115873966963b
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2014-11-04 17:14:10 -0200

    Câmera Fix
    Comentei a linha de update da câmera para ela voltar a ficar estática.
    Quem estiver responsável pela câmera, o script dela se encontra em
    "\Assets\Scripts\CameraController.cs"

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,7 +10,7 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
+		//transform.position = player.transform.position + offset;
 			
 			
 		}
