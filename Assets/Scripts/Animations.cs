015cf90cc278b20b84428c914dc309879f85ea46
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2014-11-17 21:38:59 -0200

    Player animado, fix de colisão e etc
    Esse commit é para inserir o modelo 3d do player, com as animações e o
    animator controller ja implementado. Fiz um fix nas colisões quebradas e
    uma leve mudança na câmera

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations : MonoBehaviour {
+	private Animator animator;
+	public float v;
+	public float h;
+	private bool walk = false;
+	private bool attack = false;
+	private bool defend = false;
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent ("Animator") as Animator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		v = Input.GetAxis ("Vertical");
+		h = Input.GetAxis ("Horizontal");
+
+		Walking();
+		Attacking();
+		Defending();
+
+	
+	}
+
+	void FixedUpdate(){
+
+		animator.SetFloat ("Sprint", v);
+		animator.SetBool ("Walk", walk);
+		animator.SetBool ("Attack", attack);
+		animator.SetBool ("Defend", defend);
+
+
+	}
+
+
+	void Attacking(){
+		if (Input.GetKey (KeyCode.LeftControl)) {
+			
+			attack = true;
+			
+		} else {
+			
+			attack = false;
+		}
+
+	}
+
+	void Defending(){
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			
+			defend = true;
+			
+		} else {
+			
+			defend = false;
+		}
+
+	}
+
+	void Walking(){
+		if (Input.GetKey (KeyCode.LeftAlt)) {
+
+			walk = true;
+
+		} else {
+		
+			walk = false;
+		}
+						
+	}
+
+}
