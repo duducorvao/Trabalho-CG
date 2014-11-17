c5f4d1d5e3c1d8a62484eec2eb05845b4e180c3e
Leonardo Pires <leonardo.pires@gmail.com>
2014-11-17 16:54:31 -0300

    Consertado portao

--- Assets/Scripts/ControlePortao.cs
+++ Assets/Scripts/ControlePortao.cs
@@ -5,7 +5,7 @@ public class ControlePortao : MonoBehaviour {
 	public GameObject portao;
 
 	IEnumerator LevantarPortao() {
-		for (float f = transform.position.y; f < 6; f += 0.5f) {
+		for (float f = portao.transform.position.y; f < 6; f += 0.5f) {
 			portao.transform.position = new Vector3(portao.transform.position.x, f, portao.transform.position.z);
 			yield return new WaitForSeconds(0.2f);
 		}
