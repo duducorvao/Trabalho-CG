66aa6f301680323b22774e5f45ead110c0e040f0
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2014-11-20 18:52:54 -0200

    Baú criado e outros fix
    Criado baú com plataforma, efeito de partícula e luz e posicionado no
    centro da arena. Fiz um fix da textura ta tocha que não tava aparecendo
    direito.

---
+++ Assets/Scripts/ChestAnimation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestAnimation : MonoBehaviour {
+	private Animator animator;
+	private bool open;
+	public ParticleSystem efeito;
+	public Light spotLight;
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent ("Animator") as Animator;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.O)) {
+			
+			open = true;
+			efeito.Play();
+			spotLight.gameObject.SetActive(true);
+			
+		} else {
+			
+			open = false;
+		}
+	}
+
+
+	void FixedUpdate(){
+		
+
+		animator.SetBool ("Open", open);
+		
+		
+	}
+
+}
