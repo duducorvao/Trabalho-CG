9d90e21a74cdd37e1bb4cb04fc85782907b0c3eb
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2014-11-22 15:28:03 -0200

    Poder Adicionado
    Adicionei o Poder lá no projeto e coloquei na frente do boneco pra vocês
    verem. Só apertar o shift da esquerda.

---
+++ Assets/PowerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerScript : MonoBehaviour {
+
+	public GameObject powerCollider;
+	public ParticleSystem efeito;
+	public Light pointLight;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			
+			efeito.Play ();
+			pointLight.gameObject.SetActive(true);
+
+		} else {
+
+			efeito.Stop();
+			pointLight.gameObject.SetActive(false);
+
+		}
+
+		if(efeito.isPlaying) {
+			//pointLight.gameObject.SetActive(true);
+			powerCollider.gameObject.SetActive(true);
+		}else{
+			pointLight.gameObject.SetActive(false);
+			powerCollider.gameObject.SetActive(false);
+		}
+
+	}
+}
