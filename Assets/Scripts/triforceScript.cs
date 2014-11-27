1d82e9320d8de02d09c1bf6c3cc027e94282f050
Eduardo Bruno <eduardobruno@hotmail.co.uk>
2014-11-26 22:12:57 -0200

    Algo essencial.
    Coloquei algo essencial que eu estava sentindo muita falta. Dê play e
    aperte O olhando pro baú :+1:

---
+++ Assets/Scripts/triforceScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triforceScript : MonoBehaviour {
+	private bool found;
+
+	// Use this for initialization
+	void Start () {
+
+		found = false;
+
+	}
+	
+	// Update is called once per frame
+
+	void Update(){
+
+		if (found == true) {
+			transform.Rotate(Vector3.back * 50f * Time.deltaTime, Space.Self);
+
+			if(transform.position.y <= 2){
+				transform.Translate(Vector3.up * (Time.deltaTime*0.10f), Space.World);
+
+			}
+
+		}
+
+
+
+	}
+
+
+	void FixedUpdate () {
+
+		if (Input.GetKey (KeyCode.O)) {
+			found = true;
+		}
+
+	}
+}
